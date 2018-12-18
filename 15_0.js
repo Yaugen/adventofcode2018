fd69d3f73cb4f1df6de6db0c8aff638998aa9dae
Yaugen <eugene@novoroll.com>
2018-12-18 15:42:34 +0300

    final 15

--- 15_0.js
+++ 15_0.js
@@ -16,31 +16,32 @@ const printField = (field) => {
     console.log(line.join(''));
   }
 }
-const getFieldWithData = (field, data) => {
+const getFieldWithData = (field, data = []) => {
   const { WIDTH, HEIGHT } = getSize(field);
   const emptyField = Array.from({ length: WIDTH }, () => Array.from({ length: HEIGHT }));
   for(let x = 0; x < WIDTH; x++) {
     for( let y = 0; y < HEIGHT; y++) {
-      const dataEntry = data.find(item => item.x === x && item.y == y);
-      emptyField[x][y] = dataEntry ? dataEntry.label : field[x][y];
+      const dataEntry = data.length && data.find(item => item.x === x && item.y == y);
+      emptyField[x][y] = dataEntry ? dataEntry.v : field[x][y];
     }
   }
   return emptyField;
 }
-const getInitialData = (rawData, WIDTH, HEIGHT) => {
+const getInitialData = (rawData) => {
   const goblins = [];
   const elfs = [];
-  const emptyField = Array.from({ length: WIDTH }, () => Array.from({ length: HEIGHT }));
   const raw = rawData.split('\r\n');
+  const { WIDTH, HEIGHT } = getSize(raw);
+  const emptyField = Array.from({ length: WIDTH }, () => Array.from({ length: HEIGHT }));
 
   for(let x = 0; x < WIDTH; x++) {
     for( let y = 0; y < HEIGHT; y++) {
       const cell = raw[y][x];
       if(cell === 'G') {
-        goblins.push({ x, y, hp: 300, dmg: 3, label: cell });
+        goblins.push({ x, y, hp: 200, dmg: 3, v: cell, state: 'start' });
         
       } else if (cell === 'E') {
-        elfs.push({ x, y, hp: 300, dmg: 3, label: cell });
+        elfs.push({ x, y, hp: 200, dmg: 3, v: cell, state: 'start' });
       }
       emptyField[x][y] = (cell === 'E' || cell === 'G' ? '.' : cell);
     }
@@ -48,7 +49,84 @@ const getInitialData = (rawData, WIDTH, HEIGHT) => {
   elfs.sort(sorter);
   goblins.sort(sorter);
 
-  return { elfs, goblins, emptyField };
+  return { 
+    emptyField,
+    units: [...elfs, ...goblins].sort(sorter)
+  };
+}
+
+const getNeighbours = (field, { x, y }) => {
+  const neighbours = [];
+  if(field[x] && field[x][y-1]) {
+    neighbours.push({ x, y:y-1, v: field[x][y-1]});
+  }
+  if(field[x-1] && field[x-1][y]) {
+    neighbours.push({ x: x-1, y, v: field[x-1][y]});
+  }
+  if(field[x+1] && field[x+1][y]) {
+    neighbours.push({ x: x+1, y, v: field[x+1][y]});
+  }
+  if(field[x] && field[x][y+1]) {
+    neighbours.push({ x, y:y+1, v: field[x][y+1]});
+  }
+  return neighbours;
+}
+
+const fillDistances = (field, from, current = 0) => {
+  const neighbours = getNeighbours(field, from).filter(({ v }) => v === '.' || (Number.isInteger(v) && v > current + 1 ));
+  if(neighbours.length) {
+    neighbours.forEach(n => {
+      field[n.x][n.y] = current + 1;
+      fillDistances(field, n, current + 1)
+    });
+  }
+  return field;
+}
+const getFieldWithDistances = (field, from) => {
+  const newField = getFieldWithData(field);
+  fillDistances(newField, from);
+  return newField;
+}
+
+const getReachebleUnits = (fieldWithDistances, units) => {
+  return units.map(unit => {
+    const neighbours = getNeighbours(fieldWithDistances, unit);
+    const allReachable = neighbours.filter(({ v }) => Number.isInteger(v));
+    const minReachable = allReachable.reduce((min, item) => (item.v < min.v ? item : min), { v: Number.MAX_VALUE })
+    return { ...unit, isReachable: minReachable.x ? minReachable : false };
+  }).filter(unit => unit.isReachable);
+}
+
+const getTarget = (fieldWithDistances, units) => {
+  const reachableUnits = getReachebleUnits(fieldWithDistances, units);
+  if(reachableUnits.length) {
+    return reachableUnits.reduce((target, unit) => {
+      return unit.isReachable.v < target.isReachable.v ? unit : target
+    }, { isReachable: { v: Number.MAX_VALUE } })
+  }
+  return false;
+}
+
+const getPath = (field, from, to) => {
+  const start = { x: from.x, y: from.y, v: 0, path: [] };
+  const q = [start];
+  while(q.length > 0) {
+    const current = q.shift();
+    const neighbours = getNeighbours(field, current);
+    const validNeighbours = neighbours.filter(({ v }) => Number(v) && v === current.v + 1);
+    const validWithPath = validNeighbours.map(item => ({ ...item, path: [...current.path, { x: item.x, y: item.y }] }));
+
+    const target = validWithPath.find(({ x, y }) => x === to.x && y === to.y);
+    if(target) {
+      return target.path;
+    }
+    validWithPath.forEach(item => {
+      const hasItem = q.find(qItem => qItem.x === item.x && qItem.y === item.y);
+      if(!hasItem) {
+        q.push(item);
+      }
+    });
+  }
 }
 
 module.exports = {
@@ -57,4 +135,8 @@ module.exports = {
   printField,
   getFieldWithData,
   getInitialData,
+  getNeighbours,
+  getFieldWithDistances,
+  getTarget,
+  getPath,
 }
--- 15_1.js
+++ 15_1.js
@@ -1,50 +1,132 @@
 const fs = require('fs');
-const { getInitialData, getFieldWithData, printField, sorter } = require('./15_0');
+const readline = require('readline');
+
+const { getInitialData, getFieldWithData, printField, sorter, getNeighbours, getFieldWithDistances, getTarget, getPath } = require('./15_0');
 
 const rawData = fs.readFileSync('./15_0_input.txt', 'utf8');
-const WIDTH = rawData.split('\n')[0].length - 1;
-const HEIGHT = rawData.split('\n').length;
+const { units, emptyField } = getInitialData(rawData);
+const VERBOSE = false;
 
 
-const getNeighbours = (field, { x, y }) => {
-  const neighbours = [];
-  if(field[x-1] && field[x-1][y]) {
-    neighbours.push({ x: x-1, y, v: field[x-1][y]});
-  }
-  if(field[x+1] && field[x+1][y]) {
-    neighbours.push({ x: x+1, y, v: field[x+1][y]});
-  }
-  if(field[x] && field[x][y-1]) {
-    neighbours.push({ x, y:y-1, v: field[x][y-1]});
-  }
-  if(field[x] && field[x][y+1]) {
-    neighbours.push({ x, y:y+1, v: field[x][y+1]});
-  }
-  return neighbours;
-}
-const fillDistances = (field, from, current = 0) => {
-  const neighbours = getNeighbours(field, from).filter(({ v }) => v === '.' || (Number.isInteger(v) && v >= current + 1 ));
-  if(neighbours.length) {
-    neighbours.forEach(n => {
-      field[n.x][n.y] = current + 1;
-      fillDistances(field, n, current + 1)
-    });
+const isInAttackMode = (field, unit, units) => {
+  const foes = getNeighbours(field, unit)
+    .filter(neighbour => (unit.v === 'G' && neighbour.v === 'E') || (unit.v === 'E' && neighbour.v === 'G'))
+    .map(cell => units.find(u => u.x === cell.x && u.y === cell.y));
+  const minHp = foes.reduce((minHp, foe) => Math.min(minHp, foe.hp), 200);
+  const minHpFoes = foes.filter(foe => foe.hp === minHp);
 
-  }
-}
-const {elfs, goblins, emptyField } = getInitialData(rawData, WIDTH, HEIGHT);
-const step = (unit, isElf, elfs, goblins) => {
-  const units = [...elfs, ...goblins].sort(sorter);
-  const currentField = getFieldWithData(emptyField, units);
-  fillDistances(currentField, elfs[0]);
-  const goblinsReachable = goblins.map(goblin => {
-    const neighbours = getNeighbours(currentField, goblin);
-    const reachable = neighbours.filter(({ v }) => Number.isInteger(v));
-    const minReachable = reachable.reduce((min, item) => (item.v < min.v ? item : min), { v: Number.MAX_VALUE })
-    return { ...goblin, minReachable };
-  })
-  printField(currentField);
-  console.log(goblinsReachable);
+  return minHpFoes.length ? minHpFoes[0] : false;
 }
 
-step(null, null, elfs, goblins);
+const makeAttack = (unit, units) => {
+  const fieldWithUnits = getFieldWithData(emptyField, units);
+  const attackTarget = isInAttackMode(fieldWithUnits, unit, units);
+
+  if(!attackTarget) {
+    return false;
+  }
+
+  if(VERBOSE) {
+    console.log('unit', unit, 'attack', attackTarget)
+  }
+  attackTarget.hp -= unit.dmg;
+  if(attackTarget.hp < 0 && units.indexOf(attackTarget) >= 0) {
+    units.splice(units.indexOf(attackTarget), 1);
+  }
+  unit.state = 'attack';
+  return true;
+}
+
+const makeMove = (unit, units) => {
+  const fieldWithUnits = getFieldWithData(emptyField, units);
+  const fieldWithDistances = getFieldWithDistances(fieldWithUnits, unit);
+  // printField(fieldWithDistances);
+  const foeUnits = unit.v === 'G'
+    ? units.filter(u => u.v === 'E')
+    : units.filter(u => u.v === 'G');
+  const target = getTarget(fieldWithDistances, foeUnits)
+
+  if (!target) {
+    return false;
+  }
+
+  const movePath = getPath(fieldWithDistances, unit, target.isReachable);
+  if (VERBOSE) {
+    console.log('unit', unit, 'move', movePath[0]);
+  }
+  unit.x = movePath[0].x;
+  unit.y = movePath[0].y;
+  unit.state = 'move';
+  return true;
+}
+
+const step = (unit, units) => {
+  if(unit.hp <= 0) {
+    return;
+  }
+  if (!makeAttack(unit, units)) {
+    if(makeMove(unit, units)) {
+      makeAttack(unit, units);
+    } else {
+      unit.state = 'idle';
+      if(VERBOSE) {
+        console.log('unit', unit, 'idle')
+      }
+    }
+  }
+}
+
+const round = (units) => {
+  units.sort(sorter);
+  units.slice().forEach(unit => step(unit, units))
+  const fieldWithUnits = getFieldWithData(emptyField, units);
+  if(VERBOSE) {
+    printField(fieldWithUnits);
+  }
+}
+
+const simulate = (units) => {
+  let roundCounter = 1;
+
+  while(!units.every(unit => unit.state === 'idle')) {
+    console.log(roundCounter, 'elfs:', units.filter(u => u.v === 'E').length, 'goblins:', units.filter(u => u.v === 'G').length);
+    roundCounter += 1;
+    round(units);
+  }
+  const allHp = units.reduce((acc, unit) => acc + unit.hp, 0)
+  console.log(roundCounter-3, allHp, (roundCounter-3)*allHp );
+}
+
+const part2 = () => {
+  const initialUnits = JSON.parse(JSON.stringify(units));
+  const intitalElfsCount = initialUnits.filter(u => u.v === 'E').length;
+  let str = 4;
+
+  let currentElfCount;
+  do {
+    const currentUnits = JSON.parse(JSON.stringify(initialUnits));
+    currentUnits.forEach(u => {
+      if(u.v === 'E') {
+        u.dmg = str;
+      }
+    })
+    console.log('--------------------------------------', str)
+    simulate(currentUnits);
+    currentElfCount = currentUnits.filter(u => u.v === 'E').length;
+    str += 1;
+  } while(currentElfCount !== intitalElfsCount)
+
+}
+
+// simulate(units);
+part2();
+
+
+// rl.on('line', () => {
+//   console.log('ROUND', roundCounter);
+//   roundCounter += 1;
+//   round();
+//   rl.prompt();
+// })
+
+
